9c3c4c1dd4cda1e678f6e94ca1199735626ab0f8
Sam-Sandhu <samrathjitsandhu03@gmail.com>
2023-03-10 15:49:04 -0700

    Add test

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -1,6 +1,7 @@
 package com.example.lab8;
 
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
@@ -10,7 +11,22 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
+import static org.junit.Assert.assertEquals;
+import static java.security.AccessController.getContext;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+
+
+
+
 public class CustomListTest {
-
-
+    @Test
+    public void testAddCity() {
+        CustomList customList = new CustomList(getContext(), new ArrayList<>());
+        City city = new City("TestCity", "TestProvince");
+        customList.addCity(city);
+        assertTrue(customList.getCount() == 1);
+    }
 }
